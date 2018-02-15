98113bb038e0b40ab030cf85ec81b2af08931de1
Peter Esser <peter.esser@uhs-computer.de>
2018-02-15 16:04:52 +0100

    I added SDL2 wrapper files:  - Display.h which only purpose is to open up a display, swap buffers and set the clear color.  - Input.h which is a wrapper around the keycodes of SDL2
    I also had to add the SDL2.dll and the glew32.dll in order to be able to start the program.

---
+++ TheGlassTower/src/Engine/Display.cpp
@@ -0,0 +1,87 @@
+#include "Display.h"
+
+using namespace ENGINE;
+
+Display* Display::instance = nullptr;
+Display::Display(int width, int height, const std::string title)
+	: width(width), height(height)
+{
+	Display::instance = this;
+	glewExperimental = GL_TRUE;
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cerr << "Could not init SDL2. Received following error: " << SDL_GetError() << std::endl;
+	}
+	else {
+		std::cout << "Successfully init SDL2" << std::endl;
+
+		//Request modern OpenGL context and set important attributes for OpenGL
+		SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+		SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+		//create window for rendering
+		window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
+
+		//create context for OpenGL
+		glContext = SDL_GL_CreateContext(window);
+
+		//init glew
+		GLenum res = glewInit();
+		if (res != GLEW_OK) {
+			std::cerr << "Glew failed to init..." << std::endl;
+		}
+		else {
+			glEnable(GL_DEPTH_TEST);
+			glDepthFunc(GL_LESS);
+
+
+			//print OpenGL versions
+			std::cout << "----------" << std::endl;
+			std::cout << "OpenGL is loaded:" << std::endl;
+			std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+			std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+			std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
+			std::cout << "----------" << std::endl;
+		}
+
+	}
+}
+
+Display::~Display()
+{
+	SDL_GL_DeleteContext(glContext);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+void Display::ClearBuffer()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Display::SwapBuffer()
+{
+	SDL_GL_SwapWindow(window);
+}
+
+void Display::SetClearColor(float r, float g, float b) {
+	glClearColor(r, g, b, 1.0f);
+}
+
+Display * Display::GetInstance()
+{
+	return instance;
+}
+
+void Display::SetMousePosition(int x, int y)
+{
+	SDL_WarpMouseInWindow(window, x, y);
+}
---
+++ TheGlassTower/src/Engine/Display.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <string>
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <iostream>	
+
+namespace ENGINE {
+	class Display
+	{
+	public:
+		/// <summary>
+		/// Creates a new window and a new OpenGL context 
+		/// which we need to render to.
+		/// </summary>
+		/// <param name="width">The width of the new window</param>
+		/// <param name="height">The height of the new window</param>
+		/// <param name="title">The title of the window</param>
+		Display(int width, int height, const std::string title);
+
+		~Display();
+
+		/// <summary>
+		/// Redraws the GLClearColor on the buffer and resets the depth buffer
+		/// </summary>
+		void ClearBuffer();
+
+		/// <summary>
+		/// In order to show an image/buffer we need to 
+		/// swap between two existing buffers
+		/// </summary>
+		void SwapBuffer();
+
+		/// <summary>
+		/// Returns the width of that window
+		/// </summary>
+		/// <returns>Width of the window</returns>
+		int inline GetWidth() { return width; }
+
+		/// <summary>
+		/// Returns the height of that window
+		/// </summary>
+		/// <returns>Height of the window</returns>
+		int inline GetHeight() { return height; }
+
+		/// <summary>
+		/// This call sets the clear color of the window to a certain color
+		/// </summary>
+		/// <param name="r">Red part of color (Range 0-1)</param>
+		/// <param name="g">Green part of color (Range 0-1)</param>
+		/// <param name="b">Blue part of color (Range 0-1)</param>
+		void SetClearColor(float r, float g, float b);
+
+		static Display* GetInstance();
+	private:
+		//width and height of window
+		int width, height;
+
+		//the OpenGL context needed to work with the GPU
+		SDL_GLContext glContext;
+
+		//The window handler by SDL2
+		SDL_Window * window;
+
+
+		static Display* instance;
+
+	private:
+		void SetMousePosition(int x, int y);
+	};
+}
